19c03241b8e3e568fe1bcd11eb5b215f049414ea
lunarmist <hlamere@email.sc.edu>
2023-10-13 22:22:18 -0400

    Target and Crosshair

--- Assets/TrackCrosshair.cs
+++ Assets/TrackCrosshair.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TrackCrosshair : MonoBehaviour
 {
+    public LayerMask targetLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,25 @@ public class TrackCrosshair : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mouseCursorPos;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        transform.position = mousePosition;
+
+        if (Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        Vector2 direction = Vector2.down;
+        Vector2 crosshairPosition = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(crosshairPosition, direction, Mathf.Infinity, targetLayer);
+
+        if (hit.collider != null) {
+            if (hit.collider.CompareTag("Target")) {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+        Debug.Log("Hit" + hit.collider.gameObject); 
     }
 }
